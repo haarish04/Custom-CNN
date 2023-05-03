93df0fe958e8ae90b34b9a43ae977955f27e8375
haarish04 <131394736+haarish04@users.noreply.github.com>
2023-05-04 01:48:04 +0530

    Create CNN model.py

---
+++ CNN model.py
@@ -0,0 +1,28 @@
+model = keras.models.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (50, 50, 3)), 
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
+    keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(3, activation='softmax')
+])
+model.summary()
+
+#Parameters for training Model
+alpha=0.01
+epochs=15
+optim = keras.optimizers.Adam(learning_rate=0.01)
+model.compile(optimizer = optim, loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+#Fit the model
+
+early_stopping_monitor = EarlyStopping(patience=3)
+history = model.fit(train_dataset,
+                    steps_per_epoch=len(train_dataset),
+                    epochs=epochs,
+                    validation_data=test_dataset,
+                    validation_steps=len(test_dataset),
+                    callbacks=[early_stopping_monitor])
+
