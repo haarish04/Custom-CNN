d07461ed4926b30b7f309c1867e9344e8896bbcd
haarish04 <131394736+haarish04@users.noreply.github.com>
2023-05-09 14:15:19 +0530

    Update CNN model.py

--- CNN model.py
+++ CNN model.py
@@ -1,6 +1,8 @@
 model = keras.models.Sequential([
+    #keras.layers.Conv2D(filters=16, (3,3), activation='relu', input_shape=(50,50,3)),
     keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (50, 50, 3)), 
     keras.layers.MaxPooling2D(2,2),
+    #keras.layers.BatchNormalization(axis=-1),
     keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
     keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
     keras.layers.MaxPooling2D(2,2),
